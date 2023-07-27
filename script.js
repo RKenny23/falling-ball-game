33a90682a7041c5b1430151386932f2335075ca2
RKenny23 <rkenny238@hotmail.com>
2023-07-27 19:34:24 -0400

    aider: Blocks fade away as they reach the top of the game div.
    # Aider chat conversation:
    
    # Context:
    USER: have the blocks fade away as they reach the top of the game div

--- script.js
+++ script.js
@@ -171,6 +171,11 @@ let blocks = setInterval(function () {
     if (iblockTop < 0) {
       ihole.style.animation = 'fadeout 1s forwards';
     }
+
+    // Fade out blocks when they reach the top of the game window
+    if (iblockTop < 0) {
+      iblock.style.animation = 'fadeout 1s forwards';
+    }
   }
 
   // Block collision
